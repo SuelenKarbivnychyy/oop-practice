d84db5560b4a5d79a876e7ddbee69973d0105645
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2023-11-28 17:26:05 -0500

    added extra charge for rush hours and developed a custom exception class

--- melons.py
+++ melons.py
@@ -1,4 +1,16 @@
+import random
+import datetime
+
 """Classes for melon orders."""
+
+class TooManyMelonsError(ValueError):
+
+    def __init__(self, message):  
+
+        self.message = message
+        super().__init__(self.message)     
+
+
 
 
 class AbstractMelonOrder:
@@ -12,17 +24,39 @@ class AbstractMelonOrder:
         self.shipped = False
 
 
+    def get_base_price(self):
+        """Splurge pricing, randomily choose a base price for the order."""
+
+        date_and_time = datetime.datetime.now()
+        order_time = int(date_and_time.strftime("%H"))
+        week_day_of_order = datetime.datetime.today()        
+
+        base_price = random.randrange(5, 9)
+        
+        rush_order_price = 4               
+        rush_hour = [8, 9, 10]
+        rush_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+        if order_time in rush_hour and week_day_of_order in rush_days:
+            base_price = base_price + rush_order_price
+            return base_price
+        
+        return base_price
+
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
-        self.shipped = True 
+        self.shipped = True
 
 
     def get_total(self):
         """Calculate price, including tax."""
 
-        self.base_price = 5
+        if self.qty > 100:
+            raise TooManyMelonsError("No more than 100 melons!")
+        
+        self.base_price = self.get_base_price()        
         self.christmas_melon_price = self.base_price * 1.5
 
         if 'christmas melons' in self.species:
@@ -49,7 +83,6 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
 
 
-
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
@@ -61,17 +94,14 @@ class InternationalMelonOrder(AbstractMelonOrder):
         self.tax = 0.17
 
 
-
     def __repr__(self):
 
         return f"species: {self.species}, amount: {self.qty}, shipped: {self.shipped}, order_type: {self.order_type}"      
 
 
-
     def get_total(self):
         total = super().get_total()
-        """Calculate price, including tax."""      
-    
+        """Calculate price, including tax."""     
 
         flat_fee = 3  
         
@@ -82,8 +112,6 @@ class InternationalMelonOrder(AbstractMelonOrder):
         return total
 
 
-
-
     def get_country_code(self):
         """Return the country code."""
 
@@ -91,21 +119,25 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
 
 
-# class GovernmentMelonOrder(AbstractMelonOrder):
+class GovernmentMelonOrder(AbstractMelonOrder):
 
-#     tax = 0
-#     passed_inspection = False
+    def __init__(self, species, qty):
+        super().__init__(self,species, qty)
 
-
-#     def __repr__(self):
-
-#         return f"species: {self.species}, amount: {self.qty}, shipped: {self.shipped}" 
+        self.tax = 0
+        self.passed_inspection = False
 
 
-#     def mark_inspection(self, passed):
+    def __repr__(self):
 
-#         if passed:
-#             self.passed_inspection = True
+        return f"species: {self.species}, amount: {self.qty}, shipped: {self.shipped}" 
+
+
+    def mark_inspection(self, passed):
+
+        if passed:
+            self.passed_inspection = True
+
 
 
 
